f38d85cb550cfbf7209cf0c0b7aabb034a6b6d9e
Sanne <sanne.van.mourik@capgemini.com>
2018-09-04 13:38:49 +0200

    tot en met opdracht 5

--- Controllers/GuestController.cs
+++ Controllers/GuestController.cs
@@ -14,6 +14,33 @@ namespace MyWedding.Controllers
             _dbContext = dbContext;
         }
 
-        // Custom methods here
+        [HttpPost]
+        public IActionResult Index([FromForm] string code)
+        {
+            var guest = _dbContext.Guests.FirstOrDefault(x => x.Code == code);
+
+            if (guest == null)
+            {
+                return NotFound();
+            }
+
+            return View(guest);
+        }
+
+        [HttpPost]
+        public IActionResult SaveResponse([FromForm] int id, bool isAttending, EMealType mealType, string comments)
+        {
+            var guest = _dbContext.Guests.FirstOrDefault(x => x.Id == id);
+
+            guest.IsAttending = isAttending;
+            guest.MealType = mealType;
+            guest.Comments = comments;
+            guest.HasResponded = true;
+
+            _dbContext.Guests.Update(guest);
+            _dbContext.SaveChanges();
+
+            return View();
+        }
     }
 }
